25e35f1dfeae32012b7ac5acc40956b2669e1249
oenu <51684443+oenu@users.noreply.github.com>
2022-09-28 05:38:33 -0400

    converted to use localized strings

--- stagewhisper/app/src/main.tsx
+++ stagewhisper/app/src/main.tsx
@@ -5,15 +5,20 @@ import './index.css';
 
 import App from './App';
 import Input from './views/input/Input';
+import Dashboard from './views/dashboard/Dashboard';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Input />} />
+        <Route element={<App />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/transcribe" element={<Input />}>
+            <Route index element={<Input />} />
+            {/* TODO: Replace this with a redux state */}
+          </Route>
+          <Route path="*" element={<div>404</div>} />
         </Route>
-        <Route path="*" element={<div>404</div>} />
       </Routes>
     </HashRouter>
   </React.StrictMode>,
