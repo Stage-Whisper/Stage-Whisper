2ec6ee677b6663c1aef2096c9e3e6000256d394a
oenu <51684443+oenu@users.noreply.github.com>
2022-09-28 07:07:14 -0400

    removed dead link

--- stagewhisper/app/src/views/input/Input.tsx
+++ stagewhisper/app/src/views/input/Input.tsx
@@ -11,6 +11,7 @@ function Input() {
   // Selections for the user
   const [selectedModel, setSelectedModel] = useState<string>('Base');
   const [selectedLanguage, setSelectedLanguage] = useState<string>('english');
+  const [selectedDirectory, setSelectedDirectory] = useState<string>();
   const [selectedAudio, setSelectedAudio] = useState<AudioFile[]>([
     {
       name: '',
@@ -19,24 +20,21 @@ function Input() {
   ]);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-  const [selectedDirectory, setSelectedDirectory] = useState<string>('');
+
   return (
     <>
       <SimpleGrid cols={2} breakpoints={[{ maxWidth: 900, cols: 1, spacing: 'sm' }]}>
         <Audio setSelectedAudio={setSelectedAudio} />
         <Language selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage} />
         <Model selectedModel={selectedModel} setSelectedModel={setSelectedModel} />
-        <Directory setSelectedDirectory={setSelectedDirectory} />
+        <Directory selectedDirectory={selectedDirectory} setSelectedDirectory={setSelectedDirectory} />
       </SimpleGrid>
 
       <Center my="lg">
         <Button
           onClick={() => {
-            window.ipcRenderer.send('start', {
-              model: selectedModel,
-              language: selectedLanguage,
-              audio: selectedAudio
-            });
+            // eslint-disable-next-line no-console
+            console.log(selectedModel, selectedLanguage, selectedAudio, selectedDirectory);
           }}
         >
           {strings.transcribe.submit_button}
