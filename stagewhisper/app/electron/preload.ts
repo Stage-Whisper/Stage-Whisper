8dc443f7cdfd9c335df789f7563e0370f948ea26
oenu <51684443+oenu@users.noreply.github.com>
2022-09-28 07:06:22 -0400

    ipc methods

--- stagewhisper/app/electron/preload.ts
+++ stagewhisper/app/electron/preload.ts
@@ -8,27 +8,31 @@ declare global {
 }
 
 const api = {
-  /**
-   * Here you can expose functions to the renderer process
-   * so they can interact with the main (electron) side
-   * without security problems.
-   * The function below can accessed using `window.Main.sayHello`
-   */
+  runWhisper: (args: string[]) => {
+    ipcRenderer.send('run-whisper', args);
+  },
+
+  openDirectoryDialog: async () => {
+    const result = await ipcRenderer.invoke('open-directory-dialog');
+    return result;
+  },
+
   sendMessage: (message: string) => {
     ipcRenderer.send('message', message);
   },
+
   /**
     Here function for AppBar
    */
-  Minimize: () => {
-    ipcRenderer.send('minimize');
-  },
-  Maximize: () => {
-    ipcRenderer.send('maximize');
-  },
-  Close: () => {
-    ipcRenderer.send('close');
-  },
+  // Minimize: () => {
+  //   ipcRenderer.send('minimize');
+  // },
+  // Maximize: () => {
+  //   ipcRenderer.send('maximize');
+  // },
+  // Close: () => {
+  //   ipcRenderer.send('close');
+  // },
   /**
    * Provide an easier way to listen to events
    */
