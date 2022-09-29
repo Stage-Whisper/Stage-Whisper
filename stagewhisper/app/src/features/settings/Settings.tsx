871321e26ce77db9764b4f8eb919ca2836894e56
oenu <51684443+oenu@users.noreply.github.com>
2022-09-29 11:56:10 -0400

    added responsive language picker

--- stagewhisper/app/src/features/settings/Settings.tsx
+++ stagewhisper/app/src/features/settings/Settings.tsx
@@ -1,11 +1,15 @@
-import { Card, Center, Stack } from '@mantine/core';
+import { Card, Center, Select, Stack, Switch } from '@mantine/core';
 import React from 'react';
 
 // Components
 
 // Redux
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { selectDisplayLanguage, selectTheme } from './settingsSlice';
+import { selectDisplayLanguage, selectTheme, setDisplayLanguage } from './settingsSlice';
+
+// Localization
+import strings from '../../localization';
+import { generateLanguageList } from '../input/components/language/languages';
 
 function Settings() {
   // Redux
@@ -13,10 +17,39 @@ function Settings() {
   const displayLanguage = useAppSelector(selectDisplayLanguage);
   const theme = useAppSelector(selectTheme);
 
+  // Get a list of all languages and their codes
+  const languages = generateLanguageList(displayLanguage);
+
+  // Get a list of all the languages that the app has been localized into
+  const localized = strings.getAvailableLanguages();
+
+  // Generate a list of languages to display in the dropdown
+  const languageList = languages.reduce((acc: { label: string; value: string }[], cur) => {
+    if (localized.includes(cur.code)) {
+      acc.push({
+        label: `${cur.localized} (${strings.getString(`languages.${cur.code}`, cur.code)})`,
+        value: cur.code
+      });
+    }
+    return acc;
+  }, []);
+
   return (
-    <Center my="lg">
+    <Center my="lg" style={{ maxWidth: '1200px' }}>
       <Stack>
         <Card>Setting</Card>
+        <Select
+          withinPortal
+          label={strings.transcribe?.language.prompt}
+          placeholder={strings.transcribe?.language.placeholder}
+          searchable
+          data={languageList.sort((a, b) => (a.label > b.label ? 1 : -1))}
+          onChange={(value) => {
+            if (value) {
+              dispatch(setDisplayLanguage(value));
+            }
+          }}
+        />
       </Stack>
     </Center>
   );
