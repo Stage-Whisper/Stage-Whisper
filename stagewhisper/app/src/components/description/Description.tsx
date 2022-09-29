0d6c6c24281ebf6b07f4f93672bd1386fc35b36b
oenu <51684443+oenu@users.noreply.github.com>
2022-09-29 03:27:31 -0400

    init description field

---
+++ stagewhisper/app/src/components/description/Description.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Description() {
+  return <div>Description</div>;
+}
+
+// TODO: Add Description component with name, description, and tags
+export default Description;
