6a7470a8041f4fd10e4df70c33ed854073aa5a35
oenu <51684443+oenu@users.noreply.github.com>
2022-09-29 08:14:52 -0400

    init appSlice

---
+++ stagewhisper/app/src/appSlice.ts
@@ -0,0 +1,59 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from './redux/store';
+
+// App Slice
+// This slice is used to store the state of the app
+
+enum AppView {
+  DASHBOARD = 'DASHBOARD',
+  INPUT = 'INPUT',
+  TRANSCRIBE = 'TRANSCRIBE',
+  RESULTS = 'RESULTS',
+  SETTINGS = 'SETTINGS',
+  ABOUT = 'ABOUT',
+  HELP = 'HELP',
+  ERROR = 'ERROR',
+  LOADING = 'LOADING'
+}
+
+export interface appState {
+  currentView: AppView;
+  darkMode: boolean;
+  displayLanguage: string;
+  burgerOpen: boolean;
+}
+
+const initialState: appState = {
+  currentView: AppView.DASHBOARD,
+  darkMode: false,
+  displayLanguage: 'english',
+  burgerOpen: false
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    setCurrentView: (state, action) => {
+      state.currentView = action.payload;
+    },
+    toggleDarkMode: (state) => {
+      state.darkMode = !state.darkMode;
+    },
+    setDisplayLanguage: (state, action) => {
+      state.displayLanguage = action.payload;
+    },
+    toggleBurger: (state) => {
+      state.burgerOpen = !state.burgerOpen;
+    }
+  }
+});
+
+export const { setCurrentView, toggleDarkMode, setDisplayLanguage, toggleBurger } = appSlice.actions;
+
+export const selectCurrentView = (state: RootState) => state.app.currentView;
+export const selectDarkMode = (state: RootState) => state.app.darkMode;
+export const selectDisplayLanguage = (state: RootState) => state.app.displayLanguage;
+export const selectBurgerOpen = (state: RootState) => state.app.burgerOpen;
+
+export default appSlice.reducer;
