84faf3eef90d3f8418b1c28ef36de2f9ce169855
oenu <51684443+oenu@users.noreply.github.com>
2022-09-30 14:41:04 -0400

    added more fields

--- stagewhisper/app/src/features/transcriptions/transcriptionsSlice.ts
+++ stagewhisper/app/src/features/transcriptions/transcriptionsSlice.ts
@@ -25,25 +25,31 @@ export enum transcriptionStatus {
 }
 
 export interface transcription {
-  id: number; // Unique internal id
-  title: string; // Title of the transcription
-  description: string; // Description of the transcription
-  date: string; // Date value for interview (YYYY-MM-DD)
-  created: string; // Date of when the transcription was completed (YYYY-MM-DD)
-  tags: string[]; // Tags for the transcription
+  // File information
   audio: string; // Path to the audio file
   audioTitle: string; // Title of the audio file
   audioAdded: string; // Date of when the audio file was added to the system (YYYY-MM-DD)
   length: number; // Length of the audio file in seconds
   audioFormat: string; // Format of the audio file
   language: keyof typeof WhisperLanguages; // Language of the audio file
+  date: string; // Date value for interview (YYYY-MM-DD)
+
+  // Transcription information
+  title: string; // Title of the transcription
+  description: string; // Description of the transcription
+  created: string; // Date of when the transcription was completed (YYYY-MM-DD)
+  tags: string[]; // Tags for the transcription
   translated: boolean; // Whether the transcription has been translated
   model: 'tiny' | 'base' | 'small' | 'medium' | 'large'; // Model used for the transcription
   progress: number; // Progress of the transcription
   status: transcriptionStatus; // Status of the transcription
   directory: string; // Directory where the transcription is stored
   transcript: string; // Path to the transcript file
+  transcriptLength: number; // Length of the transcript file in words
+
+  // Transcription metadata
   opened: boolean; // Whether the transcription has been opened in the editor
+  id: number; // Unique internal id
 }
 
 const initialState: transcriptionState = {
@@ -51,6 +57,7 @@ const initialState: transcriptionState = {
     {
       id: 0,
       title: 'Test Title0',
+      transcriptLength: 300,
       description: 'Test Description0',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -63,7 +70,7 @@ const initialState: transcriptionState = {
       model: 'base',
       progress: 0,
       status: transcriptionStatus.PENDING,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
       opened: false,
@@ -74,6 +81,7 @@ const initialState: transcriptionState = {
       id: 1,
       title:
         'Adventures of Sherlock Holmes - Very long title that will be truncated or break something in one of the ui views',
+      transcriptLength: 300,
       description: 'A collection of short stories by Sir Arthur Conan Doyle',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -96,7 +104,7 @@ const initialState: transcriptionState = {
       model: 'tiny',
       progress: 100,
       status: transcriptionStatus.COMPLETE,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript: `I found Renfield sitting placidly in his room with his hands folded, smiling benignly. At the moment he seemed as sane as any one I ever saw. I sat down and talked with him on a lot of subjects, all of which he treated naturally. He then, of his own accord, spoke of going home, a subject he has never mentioned to my knowledge during his sojourn here. In fact, he spoke quite confidently of getting his discharge at once. I believe that, had I not had the chat with Harker and read the letters and the dates of his outbursts, I should have been prepared to sign for him after a brief time of observation. As it is, I am darkly suspicious. All those outbreaks were in some way linked with the proximity of the Count. What then does this absolute content mean? Can it be that his instinct is satisfied as to the vampire's ultimate triumph? Stay; he is himself zoöphagous, and in his wild ravings outside the chapel door of the deserted house he always spoke of "master." This all seems confirmation of our idea. However, after a while I came away; my friend is just a little too sane at present to make it safe to probe him too deep with questions. He might begin to think, and then--! So I came away. I mistrust these quiet moods of his; so I have given the attendant a hint to look closely after him, and to have a strait-waistcoat ready in case of need. 29 September, in train to London.--When I received Mr. Billington's courteous message that he would give me any information in his power I thought it best to go down to Whitby and make, on the spot, such inquiries as I wanted. It was now my object to trace that horrid cargo of the Count's to its place in London. Later, we may be able to deal with it. Billington junior, a nice lad, met me at the station, and brought me to his father's house, where they had decided that I must stay the night. They are hospitable, with true Yorkshire hospitality: give a guest everything, and leave him free to do as he likes. They all knew that I was busy, and that my stay was short, and Mr. Billington had ready in his office all the papers concerning the consignment of boxes. It gave me almost a turn to see again one of the letters which I had seen on the Count's table before I knew of his diabolical plans. Everything had been carefully thought out, and done systematically and with precision. He seemed to have been prepared for every obstacle which might be placed by accident in the way of his intentions being carried out. To use an Americanism, he had "taken no chances," and the absolute accuracy with which his instructions were fulfilled, was simply the logical result of his care. I saw the invoice, and took note of it: "Fifty cases of common earth, to be used for experimental purposes." Also the copy of letter to Carter Paterson, and their reply; of both of these I got copies. This was all the information Mr. Billington could give me, so I went down to the port and saw the coastguards, the Customs officers and the harbour-master. They had all something to say of the strange entry of the ship, which is already taking its place in local tradition; but no one could add to the simple description "Fifty cases of common earth." I then saw the station-master, who kindly put me in communication with the men who had actually received the boxes. Their tally was exact with the list, and they had nothing to add except that the boxes were "main and mortal heavy," and that shifting them was dry work. One of them added that it was hard lines that there wasn't any gentleman "such-like as yourself, squire," to show some sort of appreciation of their efforts in a liquid form; another put in a rider that the thirst then generated was such that even the time which had elapsed had not completely allayed it. Needless to add, I took care before leaving to lift, for ever and adequately, this source of reproach. 30 September.--The station-master was good enough to give me a line to his old companion the station-master at King's Cross, so that when I arrived there in the morning I was able to ask him about the arrival of the boxes. He, too, put me at once in communication with the proper officials, and I saw that their tally was correct with the original invoice. The opportunities of acquiring an abnormal thirst had been here limited; a noble use of them had, however, been made, and again I was compelled to deal with the result in an ex post facto manner.`,
       opened: false,
       length: 100,
@@ -105,6 +113,7 @@ const initialState: transcriptionState = {
     {
       id: 2,
       title: 'Test Title2',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -117,7 +126,7 @@ const initialState: transcriptionState = {
       model: 'large',
       progress: 70,
       status: transcriptionStatus.ERROR,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
       opened: false,
@@ -127,6 +136,7 @@ const initialState: transcriptionState = {
     {
       id: 3,
       title: 'Test Title3',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -139,7 +149,7 @@ const initialState: transcriptionState = {
       model: 'medium',
       progress: 40,
       status: transcriptionStatus.STALLED,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
       opened: false,
@@ -149,6 +159,7 @@ const initialState: transcriptionState = {
     {
       id: 4,
       title: 'Test Title4',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -164,13 +175,14 @@ const initialState: transcriptionState = {
       audioFormat: 'mp3',
       progress: 20,
       status: transcriptionStatus.PAUSED,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
     },
     {
       id: 5,
       title: 'Test Title5',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -186,13 +198,14 @@ const initialState: transcriptionState = {
       audioFormat: 'mp3',
       progress: 20,
       status: transcriptionStatus.PROCESSING,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
     },
     {
       id: 6,
       title: 'Test Title6',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -208,13 +221,14 @@ const initialState: transcriptionState = {
       audioFormat: 'mp3',
       progress: 20,
       status: transcriptionStatus.CANCELLED,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
     },
     {
       id: 7,
       title: 'Test Title7',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -226,17 +240,18 @@ const initialState: transcriptionState = {
       translated: false,
       model: 'base',
       opened: false,
-      length: 100,
+      length: 10000,
       audioFormat: 'mp3',
       progress: 20,
       status: transcriptionStatus.DELETED,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
     },
     {
       id: 8,
       title: 'Test Title8',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -252,13 +267,14 @@ const initialState: transcriptionState = {
       audioFormat: 'mp3',
       progress: 20,
       status: transcriptionStatus.QUEUED,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
     },
     {
       id: 9,
       title: 'Test Title9',
+      transcriptLength: 300,
       description: 'Test Description',
       date: '2020-01-01',
       created: '2020-01-01',
@@ -274,7 +290,7 @@ const initialState: transcriptionState = {
       audioFormat: 'mp3',
       progress: 20,
       status: transcriptionStatus.IDLE,
-      directory: 'test',
+      directory: '/test/user/desktop/output.txt',
       transcript:
         'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
     }
@@ -299,11 +315,15 @@ export const transcriptionsSlice = createSlice({
       if (index !== -1) {
         state.transcriptions.splice(index, 1);
       }
+    },
+    test: (state, action) => {
+      console.log('test');
+      console.log(action.payload);
     }
   }
 });
 
-export const { addTranscription, updateTranscription, removeTranscription } = transcriptionsSlice.actions;
+export const { addTranscription, updateTranscription, removeTranscription, test } = transcriptionsSlice.actions;
 
 // Export Transcription States
 export const selectTranscriptions = (state: RootState) => state.transcriptions.transcriptions;
