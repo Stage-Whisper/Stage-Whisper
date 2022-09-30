732eddd441b08ea43d117c8f650db66cfffdd234
oenu <51684443+oenu@users.noreply.github.com>
2022-09-30 08:11:11 -0400

    added default testing data

--- stagewhisper/app/src/features/transcriptions/transcriptionsSlice.ts
+++ stagewhisper/app/src/features/transcriptions/transcriptionsSlice.ts
@@ -10,22 +10,110 @@ export interface transcriptionState {
 }
 
 export interface transcription {
-  id: string;
+  id: number;
   title: string;
   description: string;
   date: string;
   tags: string[];
   audio: string;
   language: string;
+  translated: boolean;
   model: string;
   progress: number;
-  status: string;
+  status: 'pending' | 'in progress' | 'complete';
   directory: string;
   transcript: string;
+  opened: boolean;
 }
 
 const initialState: transcriptionState = {
-  transcriptions: []
+  transcriptions: [
+    {
+      id: 0,
+      title: 'Test Title0',
+      description: 'Test Description0',
+      date: '2020-01-01',
+      tags: ['test', 'tags'],
+      audio: 'test audio 0',
+      language: 'en',
+      translated: false,
+      model: 'default model test',
+      progress: 0,
+      status: 'pending',
+      directory: 'test',
+      transcript:
+        'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
+      opened: false
+    },
+    {
+      id: 1,
+      title: 'Test Title1',
+      description: 'Test Description',
+      date: '2020-01-01',
+      tags: ['test', 'tags'],
+      audio: 'test audio',
+      language: 'en',
+      translated: false,
+      model: 'default model test',
+      progress: 0,
+      status: 'pending',
+      directory: 'test',
+      transcript:
+        'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
+      opened: false
+    },
+    {
+      id: 2,
+      title: 'Test Title2',
+      description: 'Test Description',
+      date: '2020-01-01',
+      tags: ['test', 'tags'],
+      audio: 'test audio',
+      language: 'en',
+      translated: false,
+      model: 'default model test',
+      progress: 0,
+      status: 'pending',
+      directory: 'test',
+      transcript:
+        'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
+      opened: false
+    },
+    {
+      id: 3,
+      title: 'Test Title3',
+      description: 'Test Description',
+      date: '2020-01-01',
+      tags: ['test', 'tags'],
+      audio: 'test audio',
+      language: 'en',
+      translated: false,
+      model: 'default model test',
+      progress: 0,
+      status: 'pending',
+      directory: 'test',
+      transcript:
+        'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there ',
+      opened: false
+    },
+    {
+      id: 4,
+      title: 'Test Title4',
+      description: 'Test Description',
+      date: '2020-01-01',
+      tags: ['test', 'tags'],
+      audio: 'test audio',
+      language: 'en',
+      translated: false,
+      model: 'default model test',
+      opened: false,
+      progress: 0,
+      status: 'pending',
+      directory: 'test',
+      transcript:
+        'test transcript text that will be replaced with a complicated object with timings and other things, I am filling space so that we can see what it might look like hello there '
+    }
+  ]
 };
 
 export const transcriptionsSlice = createSlice({
