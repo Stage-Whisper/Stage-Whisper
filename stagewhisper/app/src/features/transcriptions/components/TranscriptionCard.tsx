8a79c06d0826e294cce293bd440d0e5a7d657f11
oenu <51684443+oenu@users.noreply.github.com>
2022-09-30 08:11:31 -0400

    init card for testing

--- stagewhisper/app/src/features/transcriptions/components/TranscriptionCard.tsx
+++ stagewhisper/app/src/features/transcriptions/components/TranscriptionCard.tsx
@@ -1,13 +1,21 @@
-import { SimpleGrid, Center, Button } from '@mantine/core';
+import { SimpleGrid, Center, Button, Title, Card, Group, Divider } from '@mantine/core';
 import React from 'react';
+
 // Redux
 import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import { transcription } from '../transcriptionsSlice';
 
 // Localization
 import strings from '../../../localization';
 
-function TranscriptionCard() {
-  return <div>TranscriptionCard</div>;
+function TranscriptionCard({ transcription }: { transcription: transcription }) {
+  return (
+    <Card withBorder>
+      <Title order={2}>{transcription.title}</Title>
+      <Divider mt="xs" mb="md" />
+      <Title order={4}>{transcription.description}</Title>
+    </Card>
+  );
 }
 
 export default TranscriptionCard;
