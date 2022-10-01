b040db7e69ab467c79eb8de828e46ef0ff9abb8d
oenu <51684443+oenu@users.noreply.github.com>
2022-10-01 06:09:11 -0400

    init editor

---
+++ stagewhisper/app/src/features/transcriptions/components/TranscriptionEditor.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+
+// This is a component that will be used to display the transcription editor when a transcription is selected
+function TranscriptionEditor() {
+  return <div>temp</div>;
+}
+
+export default TranscriptionEditor;
