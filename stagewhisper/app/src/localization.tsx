8858f2350cf8a3f6971ea4a397d4a6fb0a45373f
oenu <51684443+oenu@users.noreply.github.com>
2022-10-01 05:24:03 -0400

    added title to localize

--- stagewhisper/app/src/localization.tsx
+++ stagewhisper/app/src/localization.tsx
@@ -156,6 +156,7 @@ export const strings = new LocalizedStrings(
       // Text for the transcriptions page which shows the user a list of all their transcriptions
       transcriptions: {
         title: 'Transcriptions', // Title of the page
+        recent_transcriptions: 'Recent Transcriptions', // Title of the recent transcriptions section
         buttons: {
           re_transcribe: 'Re-Transcribe',
           add_to_queue: 'Add to Queue'
