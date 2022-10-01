63f464ccab72cfecec983fcf159693870902ef8a
oenu <51684443+oenu@users.noreply.github.com>
2022-10-01 10:01:28 -0400

    added no transaction warning and handler

--- stagewhisper/app/src/features/transcriptions/components/TranscriptionEditor.tsx
+++ stagewhisper/app/src/features/transcriptions/components/TranscriptionEditor.tsx
@@ -8,33 +8,40 @@ import { transcription } from '../transcriptionsSlice';
 // This is a component that will be used to display the transcription editor when a transcription is selected
 function TranscriptionEditor({ active: transcription }: { active: transcription }) {
   // Get the dispatch function
-  const dispatch = useAppDispatch();
 
   // Page with a card containing the transcription metadata
 
   // Transcription Editor which shows the raw output of the model which is in VTT format
   // This will be used to edit the transcription and save it to the database
-  const transcriptionEditor = (
-    <Grid>
-      <RichTextEditor formats={[]} controls={[]} value={transcription.transcriptText} id="rte" />
-    </Grid>
-  );
 
   // This is the value of the transcription editor
 
-  return (
-    <Stack>
-      <Grid>
-        <Grid.Col md={4} sm={12}>
-          {/* Metadata 1 */}
-        </Grid.Col>
-        <Grid.Col md={8} sm={12}>
-          {/* Metadata 2 */}
-        </Grid.Col>
-      </Grid>
-      <Center>{transcriptionEditor}</Center>
-    </Stack>
-  );
+  if (!transcription) {
+    return (
+      <Center>
+        <Stack>
+          <h1> No transcription selected </h1>
+        </Stack>
+      </Center>
+    );
+  } else {
+    return (
+      <Stack>
+        <Grid>
+          <Grid.Col md={4} sm={12}>
+            {/* Metadata 1 */}
+          </Grid.Col>
+          <Grid.Col md={8} sm={12}>
+            {/* Metadata 2 */}
+          </Grid.Col>
+        </Grid>
+        <Center>
+          <Grid>
+            <RichTextEditor formats={[]} controls={[]} value={transcription.transcriptText} id="rte" />
+          </Grid>
+        </Center>
+      </Stack>
+    );
+  }
 }
-
 export default TranscriptionEditor;
