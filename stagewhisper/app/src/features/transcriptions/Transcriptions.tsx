6e0a5197eeb32210f62af07834b0b5f7d8c94f9d
oenu <51684443+oenu@users.noreply.github.com>
2022-10-01 06:10:14 -0400

    removed comments, added redirect to active page

--- stagewhisper/app/src/features/transcriptions/Transcriptions.tsx
+++ stagewhisper/app/src/features/transcriptions/Transcriptions.tsx
@@ -1,80 +1,38 @@
-import { SimpleGrid, Center, Button, Card, Divider, Table, Stack } from '@mantine/core';
+import { Stack } from '@mantine/core';
 import React from 'react';
 
 // Redux
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { useAppSelector } from '../../redux/hooks';
 
 // Components
-import { selectTranscriptions } from './transcriptionsSlice';
 import TranscriptionCard from './components/TranscriptionCard';
+import { selectActiveTranscription, selectTranscriptions } from './transcriptionsSlice';
 
 // Localization
-import strings from '../../localization';
+import TranscriptionEditor from './components/TranscriptionEditor';
 
 // Component for displaying transcription progress / results
 function Transcriptions() {
-  // Redux
-  const dispatch = useAppDispatch();
+  // Get All Transcriptions
   const transcriptions = useAppSelector(selectTranscriptions);
+
+  // Get Active Transcription (if it exists)
+  const activeId = useAppSelector(selectActiveTranscription);
 
   const transcriptionCards = transcriptions.map((transcription) => {
     return <TranscriptionCard key={transcription.id} transcription={transcription} />;
   });
 
-  return (
-    <Stack>
-      <Stack spacing="md">{transcriptionCards}</Stack>
-    </Stack>
-
-    // const tableElements = transcriptions.map((transcription) => {
-    //   return (
-    //     <tr key={transcription.id}>
-    //       <td>{transcription.id}</td>
-    //       <td>{transcription.title}</td>
-    //       <td>{transcription.description}</td>
-    //       <td>{transcription.date}</td>
-    //       <td>{transcription.tags}</td>
-    //       <td>{transcription.status}</td>
-    //       <td>{transcription.audio}</td>
-    //       <td>{transcription.language}</td>
-    //       <td>{transcription.model}</td>
-    //       <td>{transcription.progress}</td>
-    //       <td>{transcription.status}</td>
-    //       <td>{transcription.directory}</td>
-    //       <td>{transcription.transcript}</td>
-    //     </tr>
-    //   );
-    // });
-
-    // return (
-    //   <Center my="lg">
-    //     <Stack>
-    //       {/* <Card withBorder>Top explainer and column heads</Card>
-    //     <Divider /> */}
-    //       Not sure if this is working
-    //       <Table>
-    //         <thead>
-    //           <tr>
-    //             <th>ID</th>
-    //             <th>Title</th>
-    //             <th>Description</th>
-    //             <th>Date</th>
-    //             <th>Tags</th>
-    //             <th>Status</th>
-    //             <th>Audio</th>
-    //             <th>Language</th>
-    //             <th>Model</th>
-    //             <th>Progress</th>
-    //             <th>Status</th>
-    //             <th>Directory</th>
-    //             <th>Transcript</th>
-    //           </tr>
-    //         </thead>
-    //         <tbody>{tableElements}</tbody>
-    //       </Table>
-    //     </Stack>
-    //   </Center>
-  );
+  console.log(activeId);
+  if (activeId === null) {
+    return (
+      <Stack>
+        <Stack spacing="md">{transcriptionCards}</Stack>
+      </Stack>
+    );
+  } else {
+    return <TranscriptionEditor />;
+  }
 }
 
 export default Transcriptions;
