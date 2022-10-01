a83b258e4518946ce4cd0c0e5d5fa4b3755915d3
oenu <51684443+oenu@users.noreply.github.com>
2022-10-01 17:26:03 -0400

    added todo

--- stagewhisper/app/src/features/dashboard/Dashboard.tsx
+++ stagewhisper/app/src/features/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import strings from '../../localization';
 
 function Dashboard() {
-  return <Text>{strings.dashboard?.title}</Text>;
+  return <Text>{strings.dashboard?.title}</Text>; // TODO: Add dashboard content
 }
 
 export default Dashboard;
