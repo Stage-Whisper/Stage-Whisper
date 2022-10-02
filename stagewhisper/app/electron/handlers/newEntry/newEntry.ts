b4cf4c4f9840059802f48f811ade319783322f61
oenu <51684443+oenu@users.noreply.github.com>
2022-10-02 13:52:07 -0400

    init new entry handler

---
+++ stagewhisper/app/electron/handlers/newEntry/newEntry.ts
@@ -0,0 +1,77 @@
+import { entry, entryAudioParams, entryConfig } from './../getEntries/types.d';
+import { app, ipcMain, IpcMainInvokeEvent } from 'electron';
+import { v4 as uuidv4 } from 'uuid';
+import { copyFileSync, mkdirSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+// Create new entry and add it to the store
+
+type newEntryArgs = {
+  filePath: string;
+  audio: Omit<entryAudioParams, 'path' | 'addedOn'>;
+  config: Omit<entryConfig, 'created' | 'inQueue' | 'queueWeight'>;
+};
+
+export default ipcMain.handle(
+  'new-entry',
+  async (_event: IpcMainInvokeEvent, args: newEntryArgs): Promise<{ entry: entry | null; error?: string }> => {
+    const rootPath = app.getPath('userData'); // Path to the top level of the data folder
+    const storePath = join(rootPath, 'store'); // Path to the store folder
+    const dataPath = join(storePath, 'data'); // Path to the data folder
+
+    const uuid = uuidv4();
+    console.log('NewEntry: Creating new entry with UUID: ' + uuid);
+
+    try {
+      console.log('NewEntry: Copying file to data folder');
+      // Move file to data folder
+      const newFilePath = join(dataPath, uuid, 'audio', 'file' + args.filePath.split('.').pop());
+      copyFileSync(args.filePath, newFilePath); // Copy file to new location
+      console.log('NewEntry: File copied to: ' + newFilePath);
+
+      // Create entry
+      const entry: entry = {
+        uuid: uuidv4(),
+        config: {
+          inQueue: false,
+          title: args.config.title,
+          created: new Date(),
+          queueWeight: 0,
+          tags: args.config.tags
+        },
+        audio: {
+          type: args.audio.type,
+          language: args.audio.language,
+          addedOn: new Date(),
+          path: newFilePath
+        },
+        path: join(dataPath, uuid),
+        transcriptions: []
+      };
+
+      console.log('NewEntry: Creating Folder Structure');
+      // Make folders
+      const entryPath = join(dataPath, uuid);
+      mkdirSync(join(entryPath)); // Make entry folder
+      const audioPath = join(entryPath, 'audio');
+      mkdirSync(join(audioPath)); // Make audio folder
+      const transcriptionsPath = join(entryPath, 'transcriptions');
+      mkdirSync(join(transcriptionsPath)); // Make transcriptions folder
+
+      console.log('NewEntry: Writing entry to store');
+      // Make entry_config file
+      writeFileSync(join(entryPath, 'entry_config.json'), JSON.stringify(entry.config, null, 2));
+      // Make Audio Parameters file
+      writeFileSync(join(audioPath, 'parameters.json'), JSON.stringify(entry.audio, null, 2));
+
+      console.log('NewEntry: Entry created successfully');
+
+      return { entry };
+    } catch (error) {
+      console.error('NewEntry: Error creating new entry: ' + error);
+      if (error instanceof Error) {
+        return { entry: null, error: error.message };
+      } else return { entry: null, error: 'Unknown Error' };
+    }
+  }
+);
