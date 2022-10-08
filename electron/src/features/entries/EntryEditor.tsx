eedacb5e21fb06079e364e34b7cf6e64ecb3ab0b
oenu <51684443+oenu@users.noreply.github.com>
2022-10-07 23:09:26 -0400

    converted to table view

--- electron/src/features/entries/EntryEditor.tsx
+++ electron/src/features/entries/EntryEditor.tsx
@@ -1,19 +1,30 @@
 import React, { useEffect } from 'react';
 
 // Components
-import { Button, Card, Center, Group, Loader, Progress, Stack, Text, Title } from '@mantine/core';
+import {
+  Affix,
+  Button,
+  Card,
+  Center,
+  Group,
+  Loader,
+  Progress,
+  Stack,
+  Table,
+  Text,
+  Textarea,
+  TextInput,
+  Title
+} from '@mantine/core';
 // import { RichTextEditor } from '@mantine/rte';
 
 // Types
 import { Node } from 'subtitle';
 
 // Redux
-// import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
-// import { selectTranscribingStatus } from '../../whisper/whisperSlice';
-// import { selectActiveEntry, selectEntries } from '../entrySlice';
 
 // Packages
-import { IconPlayerPause, IconPlayerPlay } from '@tabler/icons';
+import { IconArrowNarrowDown, IconPlayerPause, IconPlayerPlay } from '@tabler/icons';
 import { Howl } from 'howler';
 import { useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
@@ -51,8 +62,6 @@ type formattedVTTLine = {
 
 // Construct Audio Player -- Required as will need to refresh with new audio player
 function AudioControls(audioPlayer: Howl) {
-  console.log('AudioControls: Constructing New Audio Controls');
-
   let currentPlaying = false;
   audioPlayer.on('play', () => {
     currentPlaying = true;
@@ -66,41 +75,39 @@ function AudioControls(audioPlayer: Howl) {
   return (
     <>
       {/* Create a button floating on the bottom right  */}
-      <Card style={{ position: 'absolute', right: '20px', bottom: '70px', zIndex: 99999 }} shadow="md" p="lg">
-        {/* <Text>{currentLine?.text}</Text> */}
-        <Group spacing="md">
-          {/* Button Section */}
-          {/* Check if the audio is playing in which case render. Make sure to rerender the app every time the audio finishes*/}
-
-          <Button
-            onClick={() => {
-              console.log('Play');
-              if (currentPlaying) {
-                audioPlayer.unload();
-                audioPlayer.play();
-              } else {
-                audioPlayer.play();
-              }
-            }}
-          >
-            <IconPlayerPlay></IconPlayerPlay>
-          </Button>
-          <Button
-            onClick={() => {
-              audioPlayer.pause();
-            }}
-          >
-            <IconPlayerPause></IconPlayerPause>
-          </Button>
-          <Button onClick={() => console.log(audioPlayer)}>Log Audio Player</Button>
-        </Group>
-      </Card>
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <Card shadow="md" p="lg">
+          <Group spacing="md">
+            <Button
+              onClick={() => {
+                console.debug('Play');
+                if (currentPlaying) {
+                  audioPlayer.unload();
+                  audioPlayer.play();
+                } else {
+                  audioPlayer.play();
+                }
+              }}
+            >
+              <IconPlayerPlay />
+            </Button>
+            <Button
+              onClick={() => {
+                audioPlayer.pause();
+              }}
+            >
+              <IconPlayerPause />
+            </Button>
+          </Group>
+        </Card>
+      </Affix>
     </>
   );
 }
 
 // This is a component that will be used to display the transcription editor when an entry is selected
 function EntryEditor() {
+  //#region State
   // Get the active entry id
   const dispatch = useAppDispatch();
 
@@ -119,12 +126,6 @@ function EntryEditor() {
   // Empty state to store the current line
   const [currentLine, setCurrentLine] = React.useState<formattedVTTLine | null>(null);
 
-  // Whether the page is ready to be displayed
-  const [ready, setReady] = React.useState<boolean>(false);
-
-  // The limits of the current line (in milliseconds)
-  // const [lineLimits, setLineLimits] = React.useState<[number, number]>([0, 0]);
-
   // Set up the audio player state
   const [audioPlayer, setAudioPlayer] = React.useState<Howl | null>(null);
   // const [audioPlayerArray, setAudioPlayerArray] = React.useState<Array<Howl>>([]);
@@ -137,55 +138,43 @@ function EntryEditor() {
   const [lineAudioProgress, setLineAudioProgress] = React.useState<number>(0);
   //Intervals
   const [intervalNode, setIntervalNode] = React.useState<NodeJS.Timeout | null>(null);
-  //Create a boolean
-  // const [toBeCleared, setToBeCleared] = React.useState<boolean>(false);
 
   //Get audioPadding from redux
   const audioPadding = useAppSelector(selectAudioPadding);
 
-  let content = (
-    <Center>
-      <Loader />
-    </Center>
-  );
+  const transcribingStatus = useAppSelector(selectTranscribingStatus);
+  //#endregion
 
   // Audio Control Use Effect
   // Update the audio controls when the audio player changes
   useEffect(() => {
     // If the audio player is not null
     if (audioPlayer) {
-      console.log('AudioControls: Audio Player Changed');
+      console.debug('AudioControls: Audio Player Changed');
       // Construct the audio controls
       setAudioControls(AudioControls(audioPlayer));
-      setReady(true);
     }
   }, [audioPlayer]);
 
   useEffect(() => {
-    console.log(' ---- Page Refresh ---- ');
-
     // Reset all the states when the entry changes
     setFormattedVTTLines([]);
     setCurrentLine(null);
-    setReady(false);
-    // setLineLimits([0, 0]);
     audioPlayer?.unload();
     audioPlayer?.off();
     setAudioPlayer(null);
-    // setToBeCleared(true);
     if (intervalNode) {
       clearInterval(intervalNode);
     }
     setIntervalNode(null);
     setTimeOutList([]);
     setLineAudioProgress(0);
-    console.log(' ---- Reset State ---- ');
 
-    // const loadPage = async () => {
     // If the entry has a transcription
     if (entry && entry.transcriptions[0] && entry.transcriptions[0].vtt) {
       const vttNodes = entry.transcriptions[0].vtt;
-      console.log('Got VTT Nodes: ', vttNodes);
+      // log the number of nodes
+      console.debug(`EntryEditor: ${vttNodes.length} Nodes Found`);
 
       // Format the VTT lines
       const formattedLines = [] as Array<formattedVTTLine>;
@@ -201,7 +190,7 @@ function EntryEditor() {
       });
 
       // Set the formatted lines
-      console.log('Setting Formatted Lines: ', formattedLines);
+
       setFormattedVTTLines(formattedLines);
       console.log('Got Audio Path: ', entry.audio.path);
       const audioFilePath = entry.audio.path;
@@ -228,144 +217,168 @@ function EntryEditor() {
     }
   }, [entry]);
 
-  // If the page is ready
-  if (ready) {
-    // If the entry has a transcription
-    if (entry && entry.transcriptions[0] && entry.transcriptions[0].vtt) {
-      // If the audio player is ready
-      if (audioPlayer) {
-        // Generate Quick Transcription Preview for all lines
-        const tempDisplay = formattedVTTLines.map((line) => {
-          return (
-            <Card withBorder key={line.key} my="sm">
-              <Stack spacing="md">
-                <Card withBorder>
-                  <Text italic align="center" size="lg" weight={700} style={{ fontFamily: 'Greycliff CF, sans-serif' }}>
-                    {line.text}
-                  </Text>
-                </Card>
-                <Card withBorder>
-                  <Group position="apart">
-                    <Text>
-                      {/* {line.start / 1000}s - {line.end / 1000}s */}
-                      {/* Line showing time in seconds formatted to mins */}
-                      {String(Math.floor(line.start / 1000 / 60)).padStart(2, '0')}:
-                      {String(Math.floor(line.start / 1000) % 60).padStart(2, '0')}
-                    </Text>
-
-                    <Button
-                      onClick={() => {
-                        //Play audio from 1 second
-                        setCurrentLine(line);
-                        //The amount of padding to add to the start and end of the line
-                        const playBackPadding = audioPadding as number;
-                        //The start time of the line after accounting for padding
-                        const computedLineStart = Math.max(line.start / 1000 - playBackPadding, 0);
-                        //The end time of the line after accounting for padding
-                        const computedLineEnd = Math.min(line.end / 1000 + playBackPadding, audioPlayer.duration());
-                        console.log('Computed Line Start: ', computedLineStart);
-                        console.log('Computed Line End: ', computedLineEnd);
-                        //Cancel timeouts
-                        timeOutList.forEach((timeout) => {
-                          clearTimeout(timeout);
-                        });
-                        //Cancel intervals
-                        if (intervalNode) {
-                          clearInterval(intervalNode);
-                        }
-                        setLineAudioProgress(0);
-                        audioPlayer.unload();
-                        audioPlayer.play();
-                        audioPlayer.seek(computedLineStart);
-
-                        //Every time 5% of the line is played update the progress bar setLineAudioProgressx
-                        const interval = setInterval(() => {
-                          const currentTime = audioPlayer.seek(); //in seconds
-                          const progress = (currentTime - computedLineStart) / (computedLineEnd - computedLineStart);
-                          setLineAudioProgress(progress * 100);
-                        }, 50);
-                        setIntervalNode(interval);
-
-                        //stop audio after it finishes
-                        const timeout = setTimeout(() => {
-                          audioPlayer.stop();
-                          setCurrentLine(null);
-                          clearInterval(interval);
-                        }, (computedLineEnd - computedLineStart) * 1000);
-                        setTimeOutList([...timeOutList, timeout]);
-                      }}
-                      // disabled
-                    >
-                      Play Line
-                    </Button>
-                    <Text>
-                      {' '}
-                      {String(Math.floor(line.end / 1000 / 60)).padStart(2, '0')}:
-                      {String(Math.floor(line.end / 1000) % 60).padStart(2, '0')}
-                    </Text>
-                  </Group>
-                </Card>
-                <Progress value={currentLine == line ? lineAudioProgress : 0}></Progress>
-              </Stack>
-            </Card>
-          );
-        });
-
-        content = (
-          <>
-            <Text italic align="center">
-              Known Bug: If you refresh this page the audio element loses track of files and refuses to play. To get it
-              to work use CMD-R or F5 to refresh this page in the meantime
-            </Text>
-            {audioControls}
-
-            <Title mt={'md'} align="center">
-              {entry.config.name}
-            </Title>
-            {tempDisplay}
-          </>
-        );
-      } else {
-        content = (
-          <Card shadow="md" p="lg">
-            <Text>Audio Loading...</Text>
-          </Card>
-        );
-      }
-    } else {
-      content = (
-        <Card shadow="md" p="lg">
-          <Text>No Transcription Found</Text>
-        </Card>
-      );
-    }
-  } else {
-    if (!entry?.transcriptions[0]) {
-      content = (
-        <Center>
-          <Stack>
-            <Title order={3}>No Transcription Found</Title>
+  const transcriptionTable = (formatted: formattedVTTLine[]) => {
+    if (audioPlayer) {
+      const rows = formatted.map((line) => (
+        <tr key={line.key}>
+          <td>
             <Button
               onClick={() => {
-                if (entry) {
-                  dispatch(passToWhisper({ entry }));
-                } else {
-                  console.warn("No Entry Found, can't pass to whisper");
-                  // This should eventually be a modal
+                //Play audio from 1 second
+                setCurrentLine(line);
+                //The amount of padding to add to the start and end of the line
+                const playBackPadding = audioPadding as number;
+                //The start time of the line after accounting for padding
+                const computedLineStart = Math.max(line.start / 1000 - playBackPadding, 0);
+                //The end time of the line after accounting for padding
+                const computedLineEnd = Math.min(line.end / 1000 + playBackPadding, audioPlayer.duration());
+                console.log('Computed Line Start: ', computedLineStart);
+                console.log('Computed Line End: ', computedLineEnd);
+                //Cancel timeouts
+                timeOutList.forEach((timeout) => {
+                  clearTimeout(timeout);
+                });
+                //Cancel intervals
+                if (intervalNode) {
+                  clearInterval(intervalNode);
                 }
+                setLineAudioProgress(0);
+                audioPlayer.unload();
+                audioPlayer.play();
+                audioPlayer.seek(computedLineStart);
+
+                //Every time 5% of the line is played update the progress bar setLineAudioProgress
+                const interval = setInterval(() => {
+                  const currentTime = audioPlayer.seek(); //in seconds
+                  const progress = (currentTime - computedLineStart) / (computedLineEnd - computedLineStart);
+                  setLineAudioProgress(progress * 100);
+                }, 50);
+                setIntervalNode(interval);
+
+                //stop audio after it finishes
+                const timeout = setTimeout(() => {
+                  audioPlayer.stop();
+                  setCurrentLine(null);
+                  clearInterval(interval);
+                }, (computedLineEnd - computedLineStart) * 1000);
+                setTimeOutList([...timeOutList, timeout]);
               }}
-              color="violet"
-              variant="outline"
-              disabled={transcribing.status === 'loading'}
             >
-              {strings.util.buttons?.transcribe}
+              <IconPlayerPlay />
             </Button>
-          </Stack>
-        </Center>
+          </td>
+          <td>
+            <Textarea
+              value={line.text}
+              onChange={(e) => {
+                console.log('Text Changed');
+                console.log(`Old: ${line.text}, New: ${e.target.value}`);
+              }}
+            />
+          </td>
+          <td>
+            {/* {line.start} */}
+            <Stack align="center" spacing="xs">
+              <Text>
+                {String(Math.floor(line.start / 1000 / 60)).padStart(2, '0')}:
+                {String(Math.floor(line.start / 1000) % 60).padStart(2, '0')}
+              </Text>
+              <IconArrowNarrowDown />
+              <Text>
+                {String(Math.floor(line.end / 1000 / 60)).padStart(2, '0')}:
+                {String(Math.floor(line.end / 1000) % 60).padStart(2, '0')}
+              </Text>
+            </Stack>
+          </td>
+        </tr>
+      ));
+
+      return (
+        <Table>
+          <thead>
+            <tr>
+              {/* TODO: use localized strings */}
+              <th>Preview</th>
+              <th
+                style={{
+                  width: '100%'
+                }}
+              >
+                Text
+              </th>
+              <th>Time</th>
+              {/* <th>Duration</th> */}
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
       );
+    } else {
+      return <Text>Audio Player Not Ready</Text>;
     }
+  };
+
+  // If an entry has not been passed in
+  if (!entry) {
+    return <Text>Entry Not Found</Text>;
   }
-  return content;
+
+  // If the entry has been passed in, but its transcription is currently being run
+  if (entry && transcribingStatus.entry?.config.uuid === entry.config.uuid && transcribingStatus.status === 'loading') {
+    return (
+      <Stack align={'center'} justify="center" style={{ height: '80%' }}>
+        <Title order={3}>Transcribing</Title>
+        <Loader variant="dots" />
+      </Stack>
+    );
+  }
+
+  // If an entry has been passed in but it does not have a transcription
+  if (entry && !entry.transcriptions[0]) {
+    return (
+      <Stack align={'center'} justify="center" style={{ height: '80%' }}>
+        <Title order={3}>No Transcription Found</Title>
+        <Button
+          onClick={() => {
+            if (entry) {
+              dispatch(passToWhisper({ entry }));
+            } else {
+              console.warn("No Entry Found, can't pass to whisper");
+              // This should eventually be a modal
+            }
+          }}
+          color="violet"
+          variant="outline"
+          disabled={transcribing.status === 'loading'}
+        >
+          {strings.util.buttons?.transcribe}
+        </Button>
+      </Stack>
+    );
+  }
+
+  // If an entry has been passed in and it has a transcription but the audio player is not ready
+  if (!audioPlayer) {
+    return (
+      <Stack align={'center'} justify="center" style={{ height: '80%' }}>
+        <Title order={3}>Loading</Title>
+        <Loader />
+      </Stack>
+    );
+  }
+
+  // If an entry has been passed in and it has a transcription and the audio player is ready
+  if (entry && entry.transcriptions[0] && entry.transcriptions[0].vtt) {
+    return (
+      <>
+        <Title mt={'md'} align="center">
+          {entry.config.name}
+        </Title>
+        {transcriptionTable(formattedVTTLines)}
+        {audioControls}
+      </>
+    );
+  }
 }
 
 export default EntryEditor;
