19e129fd3fbdfc63c923126033efb79e2483d628
sawhney17 <240aryans@gmail.com>
2022-10-09 19:48:01 +0400

    implement array sorting

--- electron/src/features/entries/Entries.tsx
+++ electron/src/features/entries/Entries.tsx
@@ -1,21 +1,26 @@
-import { Stack } from '@mantine/core';
-import React from 'react';
+import { Card, Stack } from '@mantine/core';
+import React, { useState } from 'react';
 
 // Redux
 import { useAppSelector } from '../../redux/hooks';
 
 // Components
 import EntryCard from './components/EntryCard';
+import FilterSettings from './components/FilterSettings';
 import { selectEntries } from './entrySlice';
 
 // Component for displaying entry progress / results
 function Entries() {
   // Get All Entries
   const entries = useAppSelector(selectEntries);
+  const [sortedEntries, setSortedEntries] = useState([...entries]);
 
   return (
     <Stack>
-      {entries.map((entry) => {
+      {/* Add filtering options */}
+      <FilterSettings entries={entries} sortedEntries={sortedEntries} setEntries={setSortedEntries}></FilterSettings>
+
+      {sortedEntries.map((entry) => {
         return <EntryCard key={entry.config.uuid} entry={entry} />;
       })}
     </Stack>
