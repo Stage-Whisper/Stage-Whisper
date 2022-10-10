0d2fae0d826bd06e988f4d4a89088e76a5133d3f
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-10-10 00:21:39 -0400

    linting fix

--- electron/src/App.tsx
+++ electron/src/App.tsx
@@ -18,7 +18,6 @@ import React, { useEffect } from 'react';
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 
 import {
-
   IconFileCheck,
   IconFileDescription,
   IconHome,
