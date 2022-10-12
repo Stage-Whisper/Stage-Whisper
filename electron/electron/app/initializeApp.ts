8e1f75b859c65c8934e5b1a0991d15a9600f4c13
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-10-11 23:44:10 -0400

    adjusted app init to match db

--- electron/electron/app/initializeApp.ts
+++ electron/electron/app/initializeApp.ts
@@ -1,19 +1,6 @@
-import { existsSync, mkdirSync, writeFileSync } from 'fs';
-import { join } from 'path';
 import { app } from 'electron';
-
-// Settings Type
-type appConfigType = {
-  darkMode: boolean;
-  language: string;
-  firstRun: boolean;
-};
-
-const defaultSettings: appConfigType = {
-  darkMode: false,
-  language: 'English',
-  firstRun: true
-};
+import { existsSync, mkdirSync } from 'fs';
+import { join } from 'path';
 
 export const initializeApp = async (): Promise<void> => {
   // Get the data path
@@ -21,7 +8,6 @@ export const initializeApp = async (): Promise<void> => {
 
   const rootPath = app.getPath('userData'); // Path to the top level the electron app - Also includes other files like localStorage, Cache, extensions, etc
   const storePath = join(rootPath, 'store'); // Path to the store folder - Where our data is stored
-  const dataPath = join(storePath, 'data'); // Path to the data folder
   const audioPath = join(storePath, 'audio'); // Path to the audio folder
 
   console.log('Store is at: ' + storePath);
