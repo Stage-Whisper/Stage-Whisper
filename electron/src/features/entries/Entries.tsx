861515e144904a1afc6a282040d20c86d4098f04
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-10-17 22:43:03 -0400

    Revert "assorted tiny fixes and bug reports (#84)" (#89)
    This reverts commit 8f66f4add5c73e35e1eeb5862727382b588724d7.

--- electron/src/features/entries/Entries.tsx
+++ electron/src/features/entries/Entries.tsx
@@ -1,26 +1,21 @@
-import { Card, Stack } from '@mantine/core';
-import React, { useState } from 'react';
+import { Stack } from '@mantine/core';
+import React from 'react';
 
 // Redux
 import { useAppSelector } from '../../redux/hooks';
 
 // Components
 import EntryCard from './components/EntryCard';
-import FilterSettings from './components/FilterSettings';
 import { selectEntries } from './entrySlice';
 
 // Component for displaying entry progress / results
 function Entries() {
   // Get All Entries
   const entries = useAppSelector(selectEntries);
-  const [sortedEntries, setSortedEntries] = useState([...entries]);
 
   return (
     <Stack>
-      {/* Add filtering options */}
-      <FilterSettings entries={entries} setEntries={setSortedEntries}></FilterSettings>
-
-      {sortedEntries.map((entry) => {
+      {entries.map((entry) => {
         return <EntryCard key={entry.config.uuid} entry={entry} />;
       })}
     </Stack>
