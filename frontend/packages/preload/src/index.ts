d0c305a89b3b35d419e6e5484f89380487368ec6
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-12-16 13:17:17 -0500

    added exports

--- frontend/packages/preload/src/index.ts
+++ frontend/packages/preload/src/index.ts
@@ -4,3 +4,40 @@
 
 export {sha256sum} from './nodeCrypto';
 export {versions} from './versions';
+
+// export const deleteStore () => ipcRenderer.invoke(Channels.DELETE_STORE)
+export {
+  deleteEntry,
+  fetchAudioFile,
+  exportTranscription,
+  runWhisper,
+  openDirectoryDialog,
+  newEntry,
+  addEntry,
+  addLine,
+  addLines,
+  addTranscription,
+  removeEntry,
+  removeLine,
+  removeTranscription,
+  restoreLine,
+  updateEntry,
+  updateLine,
+  updateTranscription,
+  getEntry,
+  getLine,
+  getLineByIndex,
+  getTranscription,
+  getEntryCount,
+  getLineCount,
+  getTranscriptionCountForEntry,
+  getTranscriptionCount,
+  getAllEntries,
+  getAllTranscriptionsForEntry,
+  getAllLinesForTranscription,
+  getAllLines,
+  getLatestLines,
+  getAllTranscriptions,
+  getAllEntriesWithTranscriptions,
+  getAllEntriesWithLinesAndTranscriptions,
+} from './api';
